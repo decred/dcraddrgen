581222dac53620fe590f29554bbd90ba1a41be10
cjepson <cjepson@decred.org>
2015-12-15 20:35:02 -0500

    Add licenses

--- base58/doc.go
+++ base58/doc.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 The btcsuite developers
+// Copyright (c) 2015 Company 0, LLC
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
--- pgpwordlist/pgpwordlist_test.go
+++ pgpwordlist/pgpwordlist_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 The decred developers
+ * Copyright (c) 2015 Company 0, LLC
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
